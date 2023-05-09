9be763b4c22ce1d5b75551e78577e25b281ebff6
Crys7a1Grain <safronov1706@mail.ru>
2023-05-10 02:56:31 +0300

    Changed the port of webserver to 80

--- index.js
+++ index.js
@@ -130,6 +130,6 @@ app.post("/search", (req, res) => {
 //   res.render('recipe', { recipe: rows[0] });
 // });
 
-app.listen(8080, () => {
-  console.log("Server started on port 8080");
+app.listen(80, () => {
+  console.log("Server started on port 80");
 });
